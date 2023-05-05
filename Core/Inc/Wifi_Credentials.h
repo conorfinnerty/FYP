34cd44e6d1cb926efee7abfa53843be6b8c9abf3
conorfinnerty <114234641+conorfinnerty@users.noreply.github.com>
2023-05-05 17:52:50 +0100

    Add files via upload

---
+++ Core/Inc/Wifi_Credentials.h
@@ -0,0 +1,15 @@
+/*
+ * Wifi_Credentials.h
+ *
+ *  Created on: Nov 11, 2022
+ *      Author: nokeeffe
+ */
+
+#ifndef INC_WIFI_CREDENTIALS_H_
+#define INC_WIFI_CREDENTIALS_H_
+
+#define SSID     "conor"
+#define PASSWORD "12345678"
+
+
+#endif /* INC_WIFI_CREDENTIALS_H_ */
